45dcc8c8f1936f1bf1719a1f9afa381a4558076d
MSchere <manu_schere@proton.me>
2023-11-19 20:04:56 +0100

    Metadata description fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 export const metadata = {
     title: "Algo Address Tracker",
-    description: "Track a wallet's Algo bnalance in real time",
+    description: "Track Algoran testnet wallet balances in real time",
     icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
